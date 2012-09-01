116ead00da55b1fba2a3d1b293b1008a96eda1f8
Romain Dardour <romain@unity.fr>
2012-09-01 21:56:00 +0200

    Settings for Notificaitons

--- application/helpers/notification_helpers.rb
+++ application/helpers/notification_helpers.rb
@@ -2,13 +2,13 @@ module Sinatra
   module NotificationHelpers
     def notify_devs(err)
       exception = err
-      from      = 'service@example.com'
+      from      = Settings.notifications.sender
       subject   = "A #{ exception.class.to_s } Occurred on Your Rack Application"
       body      = ERB.new(EMAIL_TEMPLATE).result(binding) # not sure about this (binding) thing
       html_body = ERB.new(HTML_EMAIL_TEMPLATE).result(binding)
     
       # loop and email
-      ['you@example.com'].each do |email|
+      Settings.notifications.recipients.each do |email|
         Pony.mail :to => email, :from => from, :subject => subject, :body => body, :html_body => html_body, :via => :sendmail
       end
     rescue
