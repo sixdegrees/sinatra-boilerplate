271f0fad5645504c889a47b48212192c75e18308
Romain Dardour <romain@unity.fr>
2012-09-02 12:16:35 +0200

    Sample font setup

--- application/compass.rb
+++ application/compass.rb
@@ -14,7 +14,7 @@ fonts_dir            = Settings.paths.fonts
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = false
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
